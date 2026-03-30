7a717dfebbfb714105002b5f544d05724f43abf8
Ryan-Baumann <ryan.baumann4933@gmail.com>
2026-03-30 10:46:07 -0500

    Update prim.h
    Added the stuff to prim.h file...

--- prim.h
+++ prim.h
@@ -1,1 +1,18 @@
 //Please put header information in this file.
+#ifndef primAlgs_H
+//Had these in here just be sure
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+#define primAlgs_H
+
+public:
+	//function decloration
+	//primMST should only return the totalWeight of the MST
+	//don't need printPrimMST(); 
+	int primMST();
+	
+
+#endif
